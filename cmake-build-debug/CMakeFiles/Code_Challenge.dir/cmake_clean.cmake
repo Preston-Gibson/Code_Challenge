2e620e456a6f79bbf561166e1a861b92f894192e
Preston-Gibson <95097404+Preston-Gibson@users.noreply.github.com>
2021-11-26 12:42:25 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Code_Challenge.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code_Challenge.dir/main.cpp.obj"
+  "Code_Challenge.exe"
+  "Code_Challenge.exe.manifest"
+  "Code_Challenge.pdb"
+  "libCode_Challenge.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Code_Challenge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
